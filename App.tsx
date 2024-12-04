fd17c2927ca41caab828b28408b2660d6579c1ea
nhim-21730055 <165756103+nhim-uit@users.noreply.github.com>
2024-12-04 17:38:40 +0700

    feat: initial commit and setup

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { theme } from "./theme";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>UT&apos;s GHW App</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -12,9 +13,9 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
+    alignItems: "center",
+    backgroundColor: theme.colors.white,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
   },
 });
